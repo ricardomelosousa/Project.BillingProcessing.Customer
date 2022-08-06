29107d3a346bf1360f9f890ceb2b360e07d5b89b
Ricardo de Melo Sousa <ricardo.sousa@accesscorp.com.br>
2022-08-05 22:52:31 -0300

    inclusão script sql

---
+++ Project.BillingProcessing.Customer/Project.BillingProcessing.Customer.Infra/Scripts/CustomerScript.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [Customer] (
+    [Id] INT NOT NULL IDENTITY, 	
+	[Name] varchar(400) NOT NULL,
+    [State] varchar(100) NOT NULL,
+    [Identification] bigint NOT NULL,	
+    [DateCreation] datetime NOT NULL,
+    [DateModified] datetime NULL)
