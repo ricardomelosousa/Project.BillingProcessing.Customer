809839c21de5d0e74e70e75f81192221494d2d2a
ricardomelosousa <ricardo_melo_sousa@hotmail.com>
2022-08-10 01:25:49 -0300

    Add files via upload
    .

--- Project.BillingProcessing.Customer/Project.BillingProcessing.Customer.Domain/CustomerEntity/ICustomerRepository.cs
+++ Project.BillingProcessing.Customer/Project.BillingProcessing.Customer.Domain/CustomerEntity/ICustomerRepository.cs
@@ -3,6 +3,7 @@ public interface ICustomerRepository : IRepository<Customer>
 {
     Task Create(Customer customer);
     Task<Customer> FindBy(Expression<Func<Customer, bool>> where);
+    Task<IEnumerable<CustomerEntity.Customer>> GetAll(int take);
 }
 
 
